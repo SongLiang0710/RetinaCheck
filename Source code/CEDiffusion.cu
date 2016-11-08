7d4cd44bfaba895d744f40472dc7501f64e22c0e
SongLiang0710 <neu_songliang@126.com>
2016-11-08 18:15:09 +0800

    Add files via upload

---
+++ Source code/CEDiffusion.cu
@@ -0,0 +1,268 @@
+﻿//
+// @Author	Song Liang, Northeastern University ,CN 
+//
+// The code and binary file are attributed MIT license. See the detail and latest update in 
+// https://github.com/SongLiang0710/RetinaCheck
+//
+// This copy of program is developed in Windows 7, Mathematica 11.0, CUDA Toolkit 7.5
+//
+// Functions for coherence-enhancing nonlinear diffusion algorithms.
+
+#include <math.h>
+#include "RcMathVisionParallel.h"
+
+#include "matrix3by3.cuh"
+#include "BSpline.cuh"
+
+
+__global__ void cuNonlinearCoefficient(
+    double *d_Dst,
+    double *d_Src,
+    int width,
+    int height,
+    double c
+)
+{
+    const int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int idy = blockIdx.y * blockDim.y + threadIdx.y;
+    const int idz = blockIdx.z * blockDim.z + threadIdx.z;
+    const int id = idx + idy * width + idz * width * height;
+
+    if (idx < width && idy < height)
+    {
+        double conf = d_Src[id];
+
+        if (conf <= 0) {
+            d_Dst[id] = 1;
+        }
+
+        else {
+            d_Dst[id] = exp(-conf / c); 
+        }
+    }
+}
+
+extern "C" void nonlinearDiffusionFunction(
+    double *d_Dst,
+    double *d_Src,
+    int width,
+    int height,
+    int stack,
+    double c
+)
+{
+    dim3 blocks( iDivUp(width, DEFAULT_BLOCKDIM_X), iDivUp(height, DEFAULT_BLOCKDIM_Y), iDivUp(stack,  DEFAULT_BLOCKDIM_Z));
+    dim3 threads( DEFAULT_BLOCKDIM_X, DEFAULT_BLOCKDIM_Y, DEFAULT_BLOCKDIM_Z);
+
+    cuNonlinearCoefficient<<<blocks, threads>>>( d_Dst, d_Src, width, height, c);
+}
+
+__global__ void cuNonlinearDiffusionExplicit(
+    double *d_Dst,
+    double *d_LwForward,
+    double *d_LwBackward,
+    double *d_LvForward,
+    double *d_LvBackward,
+    double *d_Lwt,
+    double *d_Dtt,
+    double *d_Dww,
+    double *d_Dvv,
+    double *d_Dwt,
+    double *d_Src,
+    int width,
+    int height,
+    int stack,
+    double tau
+)
+{
+    const int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int idy = blockIdx.y * blockDim.y + threadIdx.y;
+    const int idz = blockIdx.z * blockDim.z + threadIdx.z;
+    const int id = idx + idy * width + idz * width * height;
+
+    if (idx < width && idy < height)
+    {
+        double Ltt, Lww, Lvv, Lwt, Ltw;
+        double local = d_Src[id];
+
+        int idtF = idz < stack - 1 ? id + width * height : idx + idy * width;
+        int idtB = idz > 0 ? id - width * height : id + (stack - 1) * width * height;
+
+        Ltt = pow(stack / PI, 2) * (d_Src[idtF] - 2 * local + d_Src[idtB]);
+
+        Lww = d_LwForward[id] - 2 * local + d_LwBackward[id];
+
+        Lvv = d_LvForward[id] - 2 * local + d_LvBackward[id];
+
+        Ltw = stack / (PI * 4) * (d_LwForward[idtF] - d_LwBackward[idtF] - d_LwForward[idtB] + d_LwBackward[idtB]);
+
+        Lwt = stack / (PI * 4) * d_Lwt[id];
+
+        d_Dst[id] = local + tau * (Ltt * d_Dtt[id] + Lww * d_Dww[id] + Lvv * d_Dvv[id] + (Ltw + Lwt) * d_Dwt[id]);
+    }
+}
+
+extern "C" void OS2DDiffusionStepExplicit(
+    double *d_Dst,
+    double *d_Src,
+    int width,
+    int height,
+    int stack,
+    int k,
+    int steps,
+    double tau
+)
+{
+    int blob = width * height * stack;
+
+    double *d_LwForward, *d_LwBackward, *d_LvForward, *d_LvBackward, *d_Lwt, *d_Buf;
+    cudaMalloc((void **)&d_LwForward, blob * sizeof(double));
+    cudaMalloc((void **)&d_LwBackward, blob * sizeof(double));
+    cudaMalloc((void **)&d_LvForward, blob * sizeof(double));
+    cudaMalloc((void **)&d_LvBackward, blob * sizeof(double));
+    cudaMalloc((void **)&d_Lwt, blob * sizeof(double));
+    cudaMalloc((void **)&d_Buf, blob * sizeof(double));
+
+    dim3 blocks( iDivUp(width, DEFAULT_BLOCKDIM_X), iDivUp(height, DEFAULT_BLOCKDIM_Y), iDivUp(stack,  DEFAULT_BLOCKDIM_Z));
+    dim3 threads( DEFAULT_BLOCKDIM_X, DEFAULT_BLOCKDIM_Y, DEFAULT_BLOCKDIM_Z);
+
+    if (steps % 2 == 0)
+        cudaMemcpy(d_Dst, d_Src, blob * sizeof(double), cudaMemcpyDeviceToDevice);
+    else
+        cudaMemcpy(d_Buf, d_Src, blob * sizeof(double), cudaMemcpyDeviceToDevice);
+
+    while (steps-- > 0)
+    {
+        if (steps % 2 == 0)
+        {
+            SE2FiniteDerivativeStep( d_LwForward, d_LwBackward, d_LvForward, d_LvBackward, d_Lwt, NULL, NULL, d_Buf, width, height, stack, k);
+
+            cuNonlinearDiffusionExplicit<<<blocks, threads>>>( d_Dst, d_LwForward, d_LwBackward, d_LvForward, d_LvBackward, d_Lwt, d_Src + blob, d_Src + 2 * blob, d_Src + 3 * blob, d_Src + 4 * blob, d_Buf, width, height, stack, tau);
+        }
+
+        else
+        {
+            SE2FiniteDerivativeStep( d_LwForward, d_LwBackward, d_LvForward, d_LvBackward, d_Lwt, NULL, NULL, d_Dst, width, height, stack, k);
+
+            cuNonlinearDiffusionExplicit<<<blocks, threads>>>( d_Buf, d_LwForward, d_LwBackward, d_LvForward, d_LvBackward, d_Lwt, d_Src + blob, d_Src + 2 * blob, d_Src + 3 * blob, d_Src + 4 * blob, d_Dst, width, height, stack, tau);
+        }
+    }
+
+    cudaFree(d_LwForward);
+    cudaFree(d_LwBackward);
+    cudaFree(d_LvForward);
+    cudaFree(d_LvBackward);
+    cudaFree(d_Lwt);
+    cudaFree(d_Buf);
+}
+
+__global__ void cuCoherenceEnhancingDiffusion(
+    double *d_Dst,
+    matrix3by3 *d_Hes,
+    matrix3by3 *d_Tensor,
+    double *d_Src,
+    int width,
+    int height,
+    int stack,
+    double tau,
+    double mu
+)
+{
+    const int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int idy = blockIdx.y * blockDim.y + threadIdx.y;
+    const int idz = blockIdx.z * blockDim.z + threadIdx.z;
+    const int id = idx + idy * width + idz * width * height;
+
+    if (idx < width && idy < height)
+    {
+        matrix3by3 hes = d_Hes[id];
+        matrix3by3 tensor = d_Tensor[id];
+
+        d_Dst[id] = d_Src[id] + tau * (mu*mu * hes.D00 * tensor.D00 + mu * hes.D01 * tensor.D01 + mu * hes.D02 * tensor.D02 + mu * hes.D10 * tensor.D10 + hes.D11 * tensor.D11 + hes.D12 * tensor.D12 + mu * hes.D20 * tensor.D20 + hes.D21 * tensor.D21 + hes.D22 * tensor.D22);
+    }
+}
+
+__global__ void cuCoherenceEnhancingDiffusionTensor(
+    matrix3by3 *d_Tensor,
+    double *d_nonlinearcoef,
+    double *d_curvature,
+    double *d_deviation,
+    int width,
+    int height,
+    int stack,
+    double mu
+)
+{
+    const int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int idy = blockIdx.y * blockDim.y + threadIdx.y;
+    const int idz = blockIdx.z * blockDim.z + threadIdx.z;
+    const int id = idx + idy * width + idz * width * height;
+
+    if (idx < width && idy < height)
+    {
+        double dfh = d_deviation[id];
+        matrix3by3 RdH = make_matrix3by3(1, 0, 0, 0, cos(dfh), -sin(dfh), 0, sin(dfh), cos(dfh));
+
+        double alpha = atan(d_curvature[id] / mu);
+        matrix3by3 Qku = make_matrix3by3(cos(alpha), -sin(alpha), 0, sin(alpha), cos(alpha), 0, 0, 0, 1);
+
+        d_Tensor[id] = matrixMultiply(matrixMultiply(RdH, matrixMultiply(Qku, make_diagonal3by3(d_nonlinearcoef[id], 1, d_nonlinearcoef[id]))), transpose_matrix3by3(matrixMultiply(RdH, Qku)));
+    }
+}
+
+
+extern "C" void OS2DCoherenceEnhancingDiffusionStep(
+    double *d_Dst,
+    double *d_Src,
+    int width,
+    int height,
+    int stack,
+    int k,
+    int steps,
+    double tau,
+    double mu
+)
+{
+    int blob = width * height * stack;
+
+    matrix3by3 *d_Tensor, *d_Hes;
+
+    double *d_Buf;
+
+    cudaMalloc((void **)&d_Tensor, blob * sizeof(matrix3by3));
+    cudaMalloc((void **)&d_Hes, blob * sizeof(matrix3by3));
+    cudaMalloc((void **)&d_Buf, blob * sizeof(double));
+
+    dim3 blocks( iDivUp(width, DEFAULT_BLOCKDIM_X), iDivUp(height, DEFAULT_BLOCKDIM_Y), iDivUp(stack,  DEFAULT_BLOCKDIM_Z));
+    dim3 threads( DEFAULT_BLOCKDIM_X, DEFAULT_BLOCKDIM_Y, DEFAULT_BLOCKDIM_Z);
+
+    cuCoherenceEnhancingDiffusionTensor<<<blocks, threads>>>( d_Tensor, d_Src + blob, d_Src + 2 * blob, d_Src + 3 * blob, width, height, stack, mu);
+
+    if (steps % 2 == 0)
+        cudaMemcpy(d_Dst, d_Src, blob * sizeof(double), cudaMemcpyDeviceToDevice);
+    else
+        cudaMemcpy(d_Buf, d_Src, blob * sizeof(double), cudaMemcpyDeviceToDevice);
+
+    while (steps-- > 0)
+    {
+        if (steps % 2 == 0)
+        {
+            SE2FiniteDerivativeHessian( d_Hes, d_Buf, width, height, stack, k);
+
+            cuCoherenceEnhancingDiffusion<<<blocks, threads>>>( d_Dst, d_Hes, d_Tensor, d_Buf, width, height, stack, tau, mu);
+        }
+
+        else
+        {
+            SE2FiniteDerivativeHessian( d_Hes, d_Dst, width, height, stack, k);
+
+            cuCoherenceEnhancingDiffusion<<<blocks, threads>>>( d_Buf, d_Hes, d_Tensor, d_Dst, width, height, stack, tau, mu);
+        }
+    }
+
+    cudaFree(d_Tensor);
+    cudaFree(d_Hes);
+    cudaFree(d_Buf);
+}
+
+
---
+++ Source code/HessianFeature.cu
@@ -0,0 +1,149 @@
+﻿//
+// @Author	Song Liang, Northeastern University ,CN 
+//
+// The code and binary file are attributed MIT license. See the detail and latest update in 
+// https://github.com/SongLiang0710/RetinaCheck
+//
+// This copy of program is developed in Windows 7, Mathematica 11.0, CUDA Toolkit 7.5
+//
+// Hessian features including curvature, confidence and deviation from horizontality.
+
+#include <math.h>
+#include "RcMathVisionParallel.h"
+
+#include "matrix3by3.cuh"
+
+// Jacobian iterative method --obtaining eigenvectors of symmetric 3 by 3 matrix
+
+//#define EPSILON 0.000000000001  (the early-stopping strategy is prone to make errors and provide little improvement in speed)
+
+__device__ void jacobianEigenvector (double3 *eVector, matrix3by3 sHessian)
+{
+	matrix3by3 mEigen, mInner;
+
+	double tmp, phi, c, s; 
+	int i, j, row = 0, col = 0, iter = 0;
+
+	mEigen = make_identity3by3 ();
+
+	while (iter++ < 100) 
+	{
+		tmp = 0;
+		for (i = 0; i <= 1; i++) {
+			for (j = i + 1; j <= 2; j++) {
+				if (fabs(getEntry_matrix3by3Index(sHessian, 3 * i + j)) > tmp) {
+					row = i; 
+					col = j;
+					tmp = fabs(getEntry_matrix3by3Index(sHessian, 3 * i + j));
+				}
+			}
+		}
+		if (tmp == 0) break;
+
+		phi = -atan2(2 * getEntry_matrix3by3Index(sHessian, 3 * row + col), getEntry_matrix3by3Index(sHessian, 3 * col + col) - getEntry_matrix3by3Index(sHessian, 3 * row + row)) / 2;
+		c = cos(phi);
+		s = sin(phi);
+
+		mInner = make_identity3by3 ();
+		setEntry_matrix3by3Index(mInner, 3 * row + row, c);
+		setEntry_matrix3by3Index(mInner, 3 * row + col, s);
+		setEntry_matrix3by3Index(mInner, 3 * col + row, -s);
+		setEntry_matrix3by3Index(mInner, 3 * col + col, c);
+
+		sHessian = matrixMultiply(mInner, sHessian);
+
+		setEntry_matrix3by3Index(mInner, 3 * row + col, -s);
+		setEntry_matrix3by3Index(mInner, 3 * col + row, s);
+
+		sHessian = matrixMultiply(sHessian, mInner);
+		mEigen = matrixMultiply(mEigen, mInner);
+	}
+
+	// Arrange the eigenvector corresponding to the lowest eigenvalue at first
+	tmp = 9999;
+	for (i = 0; i < 3; i++)
+	{ 
+		if (fabs(getEntry_matrix3by3Index(sHessian, 3 * i + i)) < tmp)
+		{
+			col = i;
+			tmp = fabs(getEntry_matrix3by3Index(sHessian, 3 * i + i));
+		}
+	}
+	for (i = 0; i < 3; i++)
+	{ 
+//		tmp = rsqrt(pow(getEntry_matrix3by3Index(mEigen, col), 2) + pow(getEntry_matrix3by3Index(mEigen, col + 3), 2) + pow(getEntry_matrix3by3Index(mEigen, col + 6), 2));
+		eVector[i] = make_double3(getEntry_matrix3by3Index(mEigen, col), getEntry_matrix3by3Index(mEigen, col + 3), getEntry_matrix3by3Index(mEigen, col + 6));
+		col = (col + 1) % 3;
+	}
+}
+
+__global__ void cuHessianFeatures(
+    double3 *d_Dst,
+    matrix3by3 *d_Src,
+    int width,
+    int height,
+    int stack,
+    double mu
+)
+{
+    const int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int idy = blockIdx.y * blockDim.y + threadIdx.y;
+    const int idz = blockIdx.z * blockDim.z + threadIdx.z;
+    const int id = idx + idy * width + idz * width * height;
+
+    if (idx < width && idy < height)
+    {
+        double3 eigenVectors[3];
+
+	matrix3by3 muMatrix = make_diagonal3by3(1, mu, mu);
+        matrix3by3 Hessian = matrixMultiply(matrixMultiply(muMatrix, d_Src[id]), muMatrix);
+
+        jacobianEigenvector ( eigenVectors, matrixMultiply(transpose_matrix3by3(Hessian), Hessian));
+
+        // Curvature
+        d_Dst[id].x = eigenVectors[0].x * copysign(1., eigenVectors[0].y) * rhypot(eigenVectors[0].y,eigenVectors[0].z);
+
+        // Confidence
+        d_Dst[id].y = -vectorMultiply(vectorMatrixMultiply(eigenVectors[1], Hessian), eigenVectors[1])-vectorMultiply(vectorMatrixMultiply(eigenVectors[2], Hessian), eigenVectors[2]); 
+
+        // Deviation from horizontality
+        d_Dst[id].z = atan(eigenVectors[0].z / eigenVectors[0].y);
+    }
+}
+
+extern "C" void OS2DHessianFeatures(
+    double3 *d_Dst,
+    matrix3by3 *d_Src,
+    int width,
+    int height,
+    int stack,
+    double mu
+)
+{
+    dim3 blocks( iDivUp(width, DEFAULT_BLOCKDIM_X), iDivUp(height, DEFAULT_BLOCKDIM_Y), iDivUp(stack,  DEFAULT_BLOCKDIM_Z));
+    dim3 threads( DEFAULT_BLOCKDIM_X, DEFAULT_BLOCKDIM_Y, DEFAULT_BLOCKDIM_Z);
+
+    cuHessianFeatures<<<blocks, threads>>>( d_Dst, d_Src, width, height, stack, mu);
+}
+
+// A deprecated test function
+__global__ void cuJacobianEigenvector(
+    double3 *d_Dst,
+    matrix3by3 *d_Src
+)
+{
+    jacobianEigenvector (d_Dst, *d_Src);
+}
+
+
+extern "C" void jEigenvector(
+    matrix3by3 *d_Dst,
+    matrix3by3 *d_Src
+)
+{
+    dim3 blocks(1, 1);
+    dim3 threads(1, 1);
+
+    cuJacobianEigenvector<<<blocks, threads>>>( (double3 *)d_Dst, d_Src);
+}
+
